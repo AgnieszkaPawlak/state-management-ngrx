2bb5a53e0b0f6885b04ce8996f88d718acef41cc
AgnieszkaPawlak <agnieszka@pawlak.io>
2025-07-26 11:55:23 +0200

    remove not using imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,21 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideStore} from '@ngrx/store';
-import {reducers} from './store/reducers';
 import {Actions, provideEffects} from '@ngrx/effects';
 
 import {provideStoreDevtools} from '@ngrx/store-devtools';
 import { UsersEffects } from './store/effects/users.effects';
 import {usersReducer} from './store/reducers/users.reducer';
-import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     Actions,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore({users: usersReducer}), // Rejestracja Stanu NgRx
-    provideEffects(UsersEffects), // Rejestracja Efektów NgRx
+    provideStore({users: usersReducer}),
+    provideEffects(UsersEffects),
     provideStoreDevtools({ maxAge: 25 }),
   ],
 };
